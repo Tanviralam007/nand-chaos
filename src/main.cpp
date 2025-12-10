c76f5ea3b50808d0aecc2ab4bcb5503027fb4bf8
tanvir <tanviralam451@gmail.com>
2025-12-10 14:14:15 +0600

    feat(app): Add main application to demonstrate Nand gate

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
-#include "../include/app.hpp"
+#include <iostream>
+#include "../include/gates/Nand.hpp"
+
+using namespace nandchaos;
 
 int main(int argc, char** argv){
-    hello();
+    std::cout << "level 0: NAND Gates Demonstration\n" << std::endl;
+
+    Nand nand;
+
+    std::cout << "truth table for NAND gate:" << std::endl;
+    std::cout << "a | b | output" << std::endl;
+    std::cout << "--|---|--------" << std::endl;
+
+    for(int a = 0; a <= 1; a++){
+        for(int b = 0; b <= 1; b++){
+            nand.setInput(0, a);
+            nand.setInput(1, b);
+            nand.evaluate();
+            std::cout << a << " | " << b << " | " << nand.getOutput(0) << std::endl;
+        }
+    }
+
     return 0;
 }
