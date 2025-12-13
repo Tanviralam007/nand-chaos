4e4642968cc255f6565f5bf6e5b3e105db8a9b1d
tanvir <tanviralam451@gmail.com>
2025-12-13 19:00:07 +0600

    Implement And gate logic using NAND and Invert composition

---
+++ src/gates/And.cpp
@@ -0,0 +1,18 @@
+#include "../../include/gates/And.hpp"
+
+namespace nandchaos
+{
+    And::And() : Component(2, 1, "And"), nand() {}
+
+    void And::evaluate()
+    {
+        nand.setInput(0, inputs[0]);
+        nand.setInput(1, inputs[1]);
+        nand.evaluate();
+        
+        invert.setInput(0, nand.getOutput(0));
+        invert.evaluate();
+
+        outputs[0] = invert.getOutput(0);
+    }
+}
