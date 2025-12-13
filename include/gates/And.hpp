2a2295b35ffc75db1b565b2942ca5c570a528d04
tanvir <tanviralam451@gmail.com>
2025-12-13 18:58:36 +0600

    Add and gate header definition

---
+++ include/gates/And.hpp
@@ -0,0 +1,17 @@
+#include "../core/Component.hpp"
+#include "Nand.hpp"
+#include "Invert.hpp"
+
+namespace nandchaos
+{
+    class And : public Component
+    {
+        private:
+            Nand nand;
+            Invert invert;
+
+        public:
+            And();
+            void evaluate() override;
+    };
+}
