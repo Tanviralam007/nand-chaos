5252a0e4bd261e6cfacb20402438a9e680866e43
tanvir <tanviralam451@gmail.com>
2026-02-11 23:28:14 +0600

    implement FullAdder with cascaded HalfAdders

---
+++ include/arithmetic/FullAdder.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "../core/Component.hpp"
+#include "HalfAdder.hpp"
+#include "../gates/Or.hpp"
+
+namespace nandchaos
+{
+    class FullAdder : public Component
+    {
+        private:
+            HalfAdder halfAdder1;
+            HalfAdder halfAdder2;
+            Or orGate;
+        public:
+            FullAdder();
+            void evaluate() override;
+
+            // helper methods
+            bool getSum() const{return outputs[0];}
+            bool getCarry() const{return outputs[1];}
+    };
+}
